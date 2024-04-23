7f940f2ff6765c342a57afc9748d96f75f590dfb
Weslley araujo <weslleyadesousa@gmail.com>
2024-04-23 19:28:12 -0300

    should be able to create a new question bigger than 255 characters

--- tests/Feature/CreateQuestionTest.php
+++ tests/Feature/CreateQuestionTest.php
@@ -26,8 +26,8 @@ it('should be able to create a new question bigger than 255 characters', functio
 
 it('should check if ends with question mark ?', function () {
 
-});
+})->todo();
 
 it('should have at least 10 characters', function () {
 
-});
+})->todo();
