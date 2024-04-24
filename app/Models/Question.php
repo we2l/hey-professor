b51197e3bbacc56de7555dd2dece02b00ee3778d
Weslley araujo <weslleyadesousa@gmail.com>
2024-04-24 20:08:29 -0300

    creating seed and factory for question model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    use HasFactory;
     protected $table = 'questions';
 }
